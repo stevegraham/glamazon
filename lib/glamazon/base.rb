054d7f112142bd74ad7eb0931cc92789b3fd7752
Stevie Graham <sjtgraham@mac.com>
2010-07-24 22:26:07 +0100

    reorganized code

---
+++ lib/glamazon/base.rb
@@ -0,0 +1,40 @@
+module Glamazon
+  module Base
+    def initialize(attributes = {})
+      self.id = Digest::SHA1.hexdigest(Time.now.to_f.to_s)
+      attributes.each { |k,v| send "#{k}=", v }
+    end
+    
+    def save
+      self.class.send(:__instances).merge! id => self
+    end
+    
+    module ClassMethods
+      def find(id)
+        __instances.detect { |k,v| k == id }.last
+      end
+      
+      def all
+        __instances.map { |k,v| v }
+      end
+      
+      def create(params = {})
+        new(params).tap { |r| r.save }
+      end
+      
+      def destroy_all
+        __instances.clear
+      end
+      
+      private
+      
+      def __instances
+        @__all_instances__ ||= {}
+      end
+    end
+    
+    def self.included(base)
+      base.extend ClassMethods
+    end
+  end
+end
